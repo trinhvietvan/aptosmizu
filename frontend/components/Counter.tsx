116b8e039e9323fac0b049703e00ed825db4a1dd
vantv <trinhvietvan@gmail.com>
2024-10-11 00:05:55 +0700

    apt

--- frontend/components/Counter.tsx
+++ frontend/components/Counter.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { toast } from "@/components/ui/use-toast";
 import { getCounter } from "@/view-functions/getCounter";
-import { click } from "@/entry-functions/click";
+import { click, sendApt } from "@/entry-functions/click";
 
 export function Counter() {
   const [counter, setCounter] = useState<number>(0);
@@ -63,6 +63,27 @@ export function Counter() {
     }
   };
 
+  const onSendApt = async () => {
+    if (!account) {
+      console.error("Account or wallet client not available");
+      return;
+    }
+
+    try {
+      const committedTransaction = await signAndSubmitTransaction(sendApt());
+      const executedTransaction = await aptosClient().waitForTransaction({
+        transactionHash: committedTransaction.hash,
+      });
+      queryClient.invalidateQueries();
+      toast({
+        title: "Success",
+        description: `Transaction succeeded, hash: ${executedTransaction.hash}`,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     if (data) {
       setCounter(data.counter);
@@ -75,7 +96,7 @@ export function Counter() {
         <div className="flex flex-col gap-6">
           <h4 className="text-lg font-medium">Counter {counter}</h4>
           <Button onClick={onClickButton}>Increment counter</Button>
-          <Button onClick={onClickButton}>Send APT</Button>
+          <Button onClick={onSendApt}>Send APT</Button>
         </div>
       </CardContent>
     </Card>
--- frontend/entry-functions/click.ts
+++ frontend/entry-functions/click.ts
@@ -1,11 +1,27 @@
 import { MODULE_ADDRESS } from "@/constants";
 import { InputTransactionData } from "@aptos-labs/wallet-adapter-react";
 
+
+const APTOS_COIN = "0x1::aptos_coin::AptosCoin";
+const ADDRESS = "0x71141c00a3904a5f57bb33ad53024c360a1350568c2b7f706a848392ef6aba10";
+
+
 export const click = (): InputTransactionData => {
   return {
     data: {
       function: `${MODULE_ADDRESS}::cad_tg_counter_app::click`,
       functionArguments: [],
+    },
+  };
+};
+
+
+export const sendApt = (): InputTransactionData => {
+  return {
+    data: {
+      function: "0x1::coin::transfer",
+      typeArguments: [APTOS_COIN],
+      functionArguments: [ADDRESS, 1], // 1 is in Octas
     },
   };
 };
